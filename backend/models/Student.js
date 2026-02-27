f57c2e298530d7a387e83cbd9e7251dab2acc9db
Shreyr69 <shreyrai69@gmail.com>
2026-02-27 15:17:29 +0530

    fix: remove next() from async pre-save hook for Mongoose v8 compatibility

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -24,12 +24,11 @@ const studentSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Hash password before saving
-studentSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+// Hash password before saving (Mongoose v8+ async hooks don't use next())
+studentSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare password helper
